ab44bfd469e7b103d1913d5762302084f85439af
Héctor Somer <233424@ids.upchiapas.edu.mx>
2024-11-15 13:23:20 -0600

    Busqueda

--- routers/stand.py
+++ routers/stand.py
@@ -275,3 +275,13 @@ def get_stand_by_user_id(idseller: int, search_term: str, db: Session = Depends(
         return stands
     else:
         return False             
+@stand_router.get("/stand/search/{search_term}", status_code=status.HTTP_200_OK, response_model=List[StandResponse] | bool)
+def get_stand_by_user_id(search_term: str, db: Session = Depends(get_db)):
+    stands = db.query(StandModel).filter(
+        StandModel.name.ilike(f"%{search_term}%")
+    ).all()
+    
+    if stands:
+        return stands
+    else:
+        return False                 
