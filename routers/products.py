7daa551a8b652816e4c3a9a24ac56326361d7eb7
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-11-16 14:11:23 -0600

    fix the commit

--- routers/products.py
+++ routers/products.py
@@ -13,7 +13,6 @@ from dotenv import load_dotenv
 load_dotenv()
 product_router = APIRouter()
 
-
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
